a6184bb9a97f703d8be9370f703897b15dc79351
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-11-05 18:38:10 +1300

    Small commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 use wasm_bindgen::prelude::*;
-mod code;
 mod game;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
-}
-
-#[wasm_bindgen]
-pub fn run_python_code(code: &str) -> Result<String, JsValue> {
-    code::run_code(code)
 }
